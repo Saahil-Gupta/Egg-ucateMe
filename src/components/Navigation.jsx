3371fcf07dfed15bc70120bbf9a3ad7151687c8c
danlu <1danlu03@gmail.com>
2025-01-18 00:04:09 -0500

    navbar

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+import logo from "../assets/logo.jpg"
+
+export default function Navigation (props) {
+    return (
+        <div className="navbar">
+            <div>
+                <img className="logo" src={logo} alt="Logo" />
+            </div>
+            <div className="navbar-buttons">
+                <button className="nav-button">Home</button>
+                <button className="nav-button">About</button>
+                <button className="nav-button">Contact</button>
+            </div>
+        </div>
+    )
+}
--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,8 @@ export default defineConfig({
       proxy: {
           '/api': 'http://127.0.0.1:5000', // Proxy API requests to Flask
       },
+      watch: {
+        usePolling: true,
+      },
   },
 });
