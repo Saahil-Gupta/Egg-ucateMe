688a776346c288e36de6337b6532384abca6d640
danlu <1danlu03@gmail.com>
2025-01-18 12:17:07 -0500

    get started

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,5 +1,19 @@
+import Navigation from "./Navigation"
+
 export default function GetStarted(props) {
     return (
-        <h1>Get started</h1>
+        <>
+            <Navigation />
+            <h1 className="get-started">Get Started</h1>
+            <div className="get-started-component-wrapper">
+                <div className="get-started-component">
+                    <h2 className="header-subtitle">What do you need to learn?</h2>
+                    <div className="upload-component">
+                        <button className="upload">Import lecture slides</button>
+                        <button className="upload">Import lecture videos</button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
